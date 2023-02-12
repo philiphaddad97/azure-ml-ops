ccd0b941f218ef2d05879dc2c5bdbf8fb7224931
Philip <philiphaddad97@gmail.com>
2023-02-12 22:12:32 +0100

    fix a bug

--- deployment/register_models.sh
+++ deployment/register_models.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 az ml model register -g $(azureml.resourceGroup) -w $(azureml.workspaceName) -n svm --model-path svc.pkl -d "SVM Classification Model to Identify Keystrokes user"
 az ml model register -g $(azureml.resourceGroup) -w $(azureml.workspaceName) -n xgboost --model-path xgb_classifier.pkl -d "XGBoost Classification Model to Identify Keystrokes user"
 az ml model register -g $(azureml.resourceGroup) -w $(azureml.workspaceName) -n rf --model-path random_forest_classifier.pkl -d "Random Forest Classification Model to Identify Keystrokes user"
