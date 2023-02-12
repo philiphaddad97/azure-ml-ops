dce839f33d8f3e3ab9959de3ad0e9ec971eb548f
Philip <philiphaddad97@gmail.com>
2023-02-12 22:32:01 +0100

    Revert "fix a bug"
    This reverts commit ccd0b941f218ef2d05879dc2c5bdbf8fb7224931.

--- deployment/register_models.sh
+++ deployment/register_models.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 az ml model register -g $(azureml.resourceGroup) -w $(azureml.workspaceName) -n svm --model-path svc.pkl -d "SVM Classification Model to Identify Keystrokes user"
 az ml model register -g $(azureml.resourceGroup) -w $(azureml.workspaceName) -n xgboost --model-path xgb_classifier.pkl -d "XGBoost Classification Model to Identify Keystrokes user"
 az ml model register -g $(azureml.resourceGroup) -w $(azureml.workspaceName) -n rf --model-path random_forest_classifier.pkl -d "Random Forest Classification Model to Identify Keystrokes user"
