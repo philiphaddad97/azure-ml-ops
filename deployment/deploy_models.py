b6fccc2a5823270a20bbe602834ebe2271bb8ba6
Philip <philiphaddad97@gmail.com>
2023-02-13 14:17:06 +0100

    get models latest version

--- deployment/deploy_models.py
+++ deployment/deploy_models.py
@@ -7,6 +7,16 @@ from azureml.core.model import InferenceConfig
 from azureml.core.webservice import AciWebservice
 
 
+def get_model_latest_version(ws: Workspace, model_name: str) -> str:
+    all_versions = [
+        (model.id, model.version)
+        for model in Model.list(ws)
+        if model.id.startswith(model_name)
+    ]
+    all_versions.sort(key=lambda x: x[1], reverse=True)
+    return all_versions[0][0]
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--ws_name", type=str, help="AML workspace name")
@@ -15,6 +25,9 @@ def main():
     parser.add_argument("--tenant_id", type=str, help="Tenant id")
     parser.add_argument("--service_principal_id", type=str, help="Application id")
     parser.add_argument("--service_name", type=str, help="Endpoint service name")
+    parser.add_argument("--model_1", type=str, help="Name of the first model")
+    parser.add_argument("--model_2", type=str, help="Name of the second model")
+    parser.add_argument("--model_3", type=str, help="Name of the third model")
     parser.add_argument(
         "--azure_secret", type=str, help="Service Principal Scecret key"
     )
@@ -35,9 +48,9 @@ def main():
 
     print("Found workspace {} at location {}".format(ws.name, ws.location))
 
-    svm = Model(ws, id="svm:1")
-    xgboost = Model(ws, id="xgboost:1")
-    rf = Model(ws, id="rf:1")
+    model_1 = Model(ws, id=get_model_latest_version(ws, args.model_1))
+    model_2 = Model(ws, id=get_model_latest_version(ws, args.model_2))
+    model_3 = Model(ws, id=get_model_latest_version(ws, args.model_3))
 
     env = Environment.from_dockerfile(
         "keystrokes_env", "Dockerfile", conda_specification=None, pip_requirements=None
@@ -52,7 +65,7 @@ def main():
     service = Model.deploy(
         ws,
         aci_service_name,
-        [svm, xgboost, rf],
+        [model_1, model_2, model_3],
         inference_config,
         deployment_config,
         overwrite=True,
